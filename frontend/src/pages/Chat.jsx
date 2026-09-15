34ec2f8dfcaae5067d40055892dfeacab818fe48
Atika <aatika.amin98@gmail.com>
2026-09-16 04:23:51 +0600

    Add files via upload

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -8,6 +8,8 @@ import ChatMessage from "../components/ChatMessage";
 import PromptAnalysisCard from "../components/PromptAnalysisCard";
 import DeviceProfileCard from "../components/DeviceProfileCard";
 import RoutingDecisionCard from "../components/RoutingDecisionCard";
+import ExecutionCard from "../components/ExecutionCard";
+import OptimizationCard from "../components/OptimizationCard";
 
 import InputBox from "../components/InputBox";
 
@@ -15,53 +17,41 @@ function Chat() {
   const [messages, setMessages] = useState([]);
 
   return (
-    <div
-      className="
-      flex
-      h-screen
-      bg-[#050b18]
-      text-white
-      "
-    >
+    <div className="flex h-screen bg-[#050b18] text-white">
       <Sidebar />
 
-      <div
-        className="
-        flex-1
-        flex
-        flex-col
-        "
-      >
+      <div className="flex-1 flex flex-col">
         <Header />
 
-        <div
-          className="
-          flex-1
-          overflow-y-auto
-          px-20
-          py-10
-          "
-        >
+        <div className="flex-1 overflow-y-auto px-20 py-10">
           {messages.length === 0 ? (
             <Welcome />
           ) : (
-            messages.map((msg, index) => (
-              <div key={index}>
-                <ChatMessage role={msg.role} text={msg.text} />
+            <div className="space-y-10">
+              {messages.map((msg, index) => (
+                <div key={index} className="space-y-5">
+                  <ChatMessage role={msg.role} text={msg.text} />
 
-                {msg.prompt_analysis && (
-                  <PromptAnalysisCard data={msg.prompt_analysis} />
-                )}
+                  {msg.prompt_analysis && (
+                    <PromptAnalysisCard data={msg.prompt_analysis} />
+                  )}
 
-                {msg.device_profile && (
-                  <DeviceProfileCard data={msg.device_profile} />
-                )}
+                  {msg.device_profile && (
+                    <DeviceProfileCard data={msg.device_profile} />
+                  )}
 
-                {msg.routing_decision && (
-                  <RoutingDecisionCard data={msg.routing_decision} />
-                )}
-              </div>
-            ))
+                  {msg.routing_decision && (
+                    <RoutingDecisionCard data={msg.routing_decision} />
+                  )}
+
+                  {msg.execution && <ExecutionCard data={msg.execution} />}
+
+                  {msg.response_optimization && (
+                    <OptimizationCard data={msg.response_optimization} />
+                  )}
+                </div>
+              ))}
+            </div>
           )}
         </div>
 
